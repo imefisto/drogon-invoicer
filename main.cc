b558fd74728105b884d39d9a0ee3037c777d71df
Ignacio El Kadre <imefisto@gmail.com>
2022-04-18 19:48:00 -0300

    feat: add docroot

--- main.cc
+++ main.cc
@@ -1,7 +1,9 @@
 #include <drogon/drogon.h>
 int main() {
     //Set HTTP listener address and port
-    drogon::app().addListener("0.0.0.0",8081);
+    drogon::app()
+        .addListener("0.0.0.0",8081)
+        .setDocumentRoot("docroot");
     //Load config file
     //drogon::app().loadConfigFile("../config.json");
     //Run HTTP framework,the method will block in the internal event loop

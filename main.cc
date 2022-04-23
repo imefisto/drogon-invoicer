237b040381a209a74483a0175a8ebac19b95c00c
Ignacio El Kadre <imefisto@gmail.com>
2022-04-23 12:10:41 -0300

    feat: make port parametrizable

--- main.cc
+++ main.cc
@@ -1,8 +1,25 @@
+#include <sstream>
 #include <drogon/drogon.h>
-int main() {
+
+int main(int argc, char *argv[]) {
+    int port = 80;
+
+    if (argc >= 2)
+    {
+        std::istringstream iss(argv[1]);
+        int val;
+
+        if (iss >> val)
+        {
+            port = val;
+        }
+    }
+
+    std::cout << "Starting app listening port " << port << std::endl;
+
     //Set HTTP listener address and port
     drogon::app()
-        .addListener("0.0.0.0",8081)
+        .addListener("0.0.0.0", port)
         .setDocumentRoot("docroot");
     //Load config file
     //drogon::app().loadConfigFile("../config.json");
